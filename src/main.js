c6de42fd6938938e3331b8631615cbca8261f6cc
linxingchen <linxingchen@iwaimai.baidu.com>
2017-06-19 20:19:17 +0800

    fix bugs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui/dist/theme-light.css'
 import Vuex from 'vuex'
 
 import App from './App'
--- src/pages/register/components/RegStep1.vue
+++ src/pages/register/components/RegStep1.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="reg_step_1">
-        <mu-text-field label="手机号" v-model="reg_phone" labelFloat/>
-        <mu-raised-button label="发送验证码"  class="reg_phone_captcha_btn" v-if="reg_phone_captcha_send == false" @click="send_phone_captcha()"/>
-        <mu-raised-button label="验证码已发送" disabled="disabled" class="reg_phone_captcha_btn" v-if="reg_phone_captcha_send == true"/>
-        <br/>
+        <mu-row gutter>
+            <mu-text-field label="手机号" v-model="reg_phone" labelFloat />
+            <mu-raised-button label="发送验证码"  class="reg_phone_captcha_btn" v-if="reg_phone_captcha_send == false" @click="send_phone_captcha()"/>
+            <mu-raised-button label="验证码已发送" disabled="disabled" class="reg_phone_captcha_btn" v-if="reg_phone_captcha_send == true"/>
+        </mu-row>
         <mu-text-field label="验证码" v-model="reg_phone_captcha" class="reg_phone_captcha_input" labelFloat/>
         <br/>
-        <mu-raised-button label="完善信息" fullWidth primary/>
+        <mu-raised-button label="完善信息" class="reg_step_2_btn" fullWidth primary/>
 
     </div>
 </template>
@@ -51,14 +52,16 @@
     .reg_step_1 {
         display: inline-block;
         width: auto;
+        margin: 5px 5px 5px 5px;
     }
     .reg_phone_captcha_btn {
         margin-left: 10px;
+        margin-top:  20px;
     }
     .reg_phone_captcha_input {
         width: 100%;
     }
-    .mu-raised-button {
+    .reg_step_2_btn {
         margin-bottom: 20px;
     }
 </style>
